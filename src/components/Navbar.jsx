74ea22b19aaa4b966dbc3886ab22642a370fe771
annafricks <annafricks33@gmail.com>
2024-05-23 16:33:42 -0400

    lab done, anna and shamil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,32 @@
+import { NavLink } from "react-router-dom";
 import logo from "./../assets/logo-ironhack-blue.png";
 
 function Navbar() {
   return (
     <nav className="bg-blue-600 text-white shadow-md fixed top-0 left-0 w-full z-50">
+      <ul>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          >
+            <li></li>
+          </NavLink>
+          <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          >
+            <li></li>
+          </NavLink>
+      </ul>
+
       <div className="flex justify-between h-20 items-center px-4">
         <div className="flex items-center space-x-2 w-1/4">
 
           {/* Home Button (Logo) */}
           <button className="flex items-center text-l py-1">
+            <NavLink to={"/"}>
             <img src={logo} alt="Logo" className="h-8 w-auto" />
+            </NavLink>
           </button>
         </div>
 
@@ -21,13 +39,14 @@ function Navbar() {
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
           <button className="flex items-center text-l py-1">
+            <NavLink to={"/profile"}>
             <img
               src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
               alt="Logo"
               className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
             />
+            </NavLink>
           </button>
-
         </div>
       </div>
     </nav>
